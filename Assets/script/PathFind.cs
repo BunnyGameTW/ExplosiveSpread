39843427a1092f8cd8c1d4d3359f4f35bec860f7
Angel Cheng <c6150261602@yahoo.com.tw>
2019-04-13 18:13:25 +0800

    add test scene

--- Assets/script/PathFind.cs
+++ Assets/script/PathFind.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 //
 public class PathFind : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    float reachDistance = 10;
+    public Transform[] targets;
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        findTarget();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (agent.remainingDistance <= reachDistance) {
+            findTarget();
+        }
 
+    }
+
+    // 到達之後就往某個地方轉向 放個特效?
+    void findTarget()
+    {
+        agent.destination = targets[Random.Range(0, targets.Length)].position;
+
+    }
 }
